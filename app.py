e5f0351f0ea3e4ad8c8ed9be02ee7c70a6aadca1
thomasnie <thomasnie@uia.no>
2021-12-13 03:56:22 +0100

    Added RARBG api

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
+import rarbgapi
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def json():
-    return render_template('index.html')
+@app.route('/<string:name>/', methods=['GET'])
+def hello_word(name):
+    client = rarbgapi.RarbgAPI()
+    titles = client.search(search_imdb=name, extended_response=True, sort="seeders",
+                           categories=[rarbgapi.RarbgAPI.CATEGORY_MOVIE_H264_1080P,
+                                       rarbgapi.RarbgAPI.CATEGORY_MOVIE_BD_REMUX])
 
+    torrent = dict()
 
-@app.route('/background_process_test')
-def background_process_test():
-    return ("nothing")
+    for i in range(len(titles)):
+        torrent[i] = ({"filename": titles[i].filename,
+                       "size": round((titles[i].size / 1073741824), 2),
+                       "category": titles[i].category,
+                       "seeders": titles[i].seeders,
+                       "magnet": titles[i].download
+                       })
+
+    return torrent
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- templates/request.js
+++ templates/request.js
@@ -9,19 +9,21 @@ const settings = {
     }
 };
 
-$("#title").keyup(function (event) {
+// Need fix
+$("#title").keyup(function(event) {
     if (event.keyCode === 13) {
         $("#titleButton").click();
     }
 });
 
-
 function getTitleValue() {
     const container = document.getElementById('imageContainer');
+    // Remove previous search
     container.textContent = '';
     // Selecting the input element and get its value
-    var inputVal = document.getElementById("title").value;
+    const inputVal = document.getElementById("title").value;
 
+    // Removes unnecessary spaces
     var newStr = inputVal.replaceAll(" ", "%");
     while (newStr[newStr.length - 1] === "%") {
         newStr = newStr.slice(0, -1);
@@ -29,19 +31,22 @@ function getTitleValue() {
 
     settings.url = `https://imdb8.p.rapidapi.com/auto-complete?q=${newStr}`;
 
-    $.getJSON(settings, function api(result) {
-        let text = "";
+    $.getJSON(settings, function (result) {
         for (let i = 0; i < result['d'].length; i++) {
             if (result['d'][i]['q'] === "TV series" || result['d'][i]['q'] === "feature") {
                 const element = document.createElement("div");
 
-                // Find the title image
+                // Find the image
                 const img = document.createElement('img');
                 img.src = result['d'][i]['i']["imageUrl"];
+                img.alt = result['d'][i]['id']
+                img.onclick = function () {
+                    titleValue(event)
+                };
 
                 // Find the title
                 const para = document.createElement("p");
-                para.innerText = result['d'][i]['l']
+                para.innerText = result['d'][i]['l'];
                 container.appendChild(para);
 
                 // Append image and title into div
@@ -50,4 +55,16 @@ function getTitleValue() {
             }
         }
     });
+}
+
+// Need fix, can't read JSON
+function titleValue(event) {
+    const container = document.getElementById('imageContainer');
+    container.textContent = '';
+
+    const alt = event.target.alt;
+
+    $.ajax({url: `http://127.0.0.1:5000/${alt}/`, success: function(result){
+      alert(result);
+    }});
 }
