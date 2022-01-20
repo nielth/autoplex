f780a7d6c171eff6e489e12605eb5a011aa78424
nielth <thomas.nielsen98@gmail.com>
2022-01-20 17:16:21 +0100

    Deletes downloaded torrents (not files)

--- torrent.py
+++ torrent.py
@@ -1,17 +1,34 @@
+import threading
 import qbittorrentapi
+import time
+
 
 # instantiate a Client using the appropriate WebUI configuration
 qbt_client = qbittorrentapi.Client(
     host='10.0.0.33',
-    port=8081,
+    port=8080,
     username='admin',
     password='adminadmin',
 )
 
+lock = threading.Lock()
+
 
 def torrentAPI(magnet, category):
     if category == "movie":
-        qbt_client.torrents_add(magnet, save_path='/downloads/movies', is_sequential_download=True)
+        qbt_client.torrents_add(
+            magnet, save_path='/downloads/movies', is_sequential_download=True)
     elif category == "series":
-        qbt_client.torrents_add(magnet, save_path='/downloads/tvseries', is_sequential_download=True)
+        qbt_client.torrents_add(
+            magnet, save_path='/downloads/tvseries', is_sequential_download=True)
 
+
+def deleteFinished():
+    while(True):
+        time.sleep(30)
+        with lock:
+            for torrent in qbt_client.torrents_info():
+                # check if torrent is downloading
+                if torrent.state_enum.is_complete:
+                    qbt_client.torrents_delete(
+                        delete_files=False, torrent_hashes=torrent.hash)
--- web/request.js
+++ web/request.js
@@ -6,31 +6,30 @@
 #############################
 */
 
-let movie = window.location.pathname
-
-
 $("#title").keyup(function (event) {
     if (event.keyCode === 13) {
         $("#titleButton").click();
     }
 });
 
+
 $('input[type="checkbox"]').on('change', function () {
     $('input[type="checkbox"]').not(this).prop('checked', false);
 });
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     $("input[name='movie']").attr('checked', 'checked');
 });
 
 
-document.getElementById("titleButton").addEventListener("click", function () {    
+document.getElementById("titleButton").addEventListener("click", function () {
 
     const inputVal = document.getElementById("title").value;
 
-    if(document.getElementById("movie").checked){
+    if (document.getElementById("movie").checked) {
         location.href = `/search/movie/${inputVal}`;
-    } else if(document.getElementById("series").checked){
+    } else if (document.getElementById("series").checked) {
         location.href = `/search/series/${inputVal}`;
     } else {
         alert("Choose movie or tv series");
@@ -42,8 +41,7 @@ function torrentDownload(magnet) {
     const magnet_link = {
         'magnet': magnet,
     };
-    let urlCategory = "";
-    urlCategory = window.location.pathname;
+    let urlCategory = window.location.pathname;
 
     $.ajax({
         url: urlCategory,
