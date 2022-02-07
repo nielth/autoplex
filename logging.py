b0fb4dfaae57a1d0b99173924a8495d4805db94c
nielth <thomas.nielsen98@gmail.com>
2022-02-07 13:33:56 +0100

    json loggin

---
+++ logging.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import json
+
+def download_log(magnet_link, category):
+    with open('downloaded.json', 'r+') as file:
+        logged_download = {
+            "time": datetime.today().strftime('%H:%M:%S'),
+            "category": category,
+            "title": magnet_link['title'],
+            "magnet": magnet_link['magnet']
+        }
+        try:
+            file_data = json.load(file)
+            file_data[datetime.today().strftime('%Y-%m-%d')
+                    ].append(logged_download)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+        except json.decoder.JSONDecodeError:
+            logged_download_new = {
+                datetime.today().strftime('%Y-%m-%d'): [logged_download],
+            }
+            json.dump(logged_download_new, file, indent=4)
