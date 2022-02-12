1cd1e7b737734fe4f505e3bc191cde66eac50105
nielth <thomas.nielsen98@gmail.com>
2022-02-12 02:50:13 +0100

    fixed dictionary

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import rarbgapi
 import time
+import os
 
 from flask import Flask, request, render_template
 from imdb import IMDb
@@ -20,15 +21,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/search/<string:category>/<string:temp>/", methods=["GET"])
-def search(category, temp):
+@app.route("/search/<string:category>/<string:title_search>/", methods=["GET"])
+def search(category, title_search):
     return_category = category
     if category == "series":
         category = "tv " + category
-    movie = ia.search_movie(temp)
-    title = list()
-    cover = list()
-    id = list()
+    movie = ia.search_movie(title_search)
+    info_list = dict()
     titles = 0
 
     for i in range(len(movie)):
@@ -39,20 +38,18 @@ def search(category, temp):
             and "._V1_" in movie[i].data["cover url"]
         ):
             result = movie[i].data["cover url"].find("._V1_")
-            title.append(movie[i].data["title"])
-            cover.append(
-                movie[i]
-                .data["cover url"]
-                .replace(movie[i].data["cover url"][result + 3: result + 23], "")
-            )
-            id.append(movie[i].movieID)
+            info_list.setdefault("title", []).append(movie[i].data["title"])
+            info_list.setdefault("cover", []).append(movie[i]
+                                                     .data["cover url"]
+                                                     .replace(movie[i].data["cover url"][result + 3: result + 23], ""))
+            info_list.setdefault("id", []).append(movie[i].movieID)
             titles += 1
     return render_template(
         "index.html",
-        len=len(title),
-        title=title,
-        cover=cover,
-        id=id,
+        len=len(info_list["title"]),
+        title=info_list["title"],
+        cover=info_list["cover"],
+        id=info_list["id"],
         category=return_category,
     )
 
@@ -84,10 +81,7 @@ def get_magnet(name, category):
 
 @app.route("/torrent/<string:category>/<string:name>/", methods=["GET"])
 def get_torrents(name, category):
-    filename = list()
-    size = list()
-    seeders = list()
-    magnet = list()
+    info_list = dict()
     title = ia.get_movie(name)
     result = title.data["cover url"].find("._V1_")
     cover = title.data["cover url"].replace(
@@ -111,35 +105,34 @@ def get_torrents(name, category):
     f = open("magnets.md", "a")
     for i in range(len(titles)):
         if titles[i].seeders != 0:
-            filename.append(titles[i].filename)
-            size.append(round((titles[i].size / 1073741824), 2))
-            seeders.append(titles[i].seeders)
-            magnet.append(titles[i].download)
+            info_list.setdefault("filename", []).append(titles[i].filename)
+            info_list.setdefault("size", []).append(
+                round((titles[i].size / 1073741824), 2))
+            info_list.setdefault("seeders", []).append(titles[i].seeders)
+            info_list.setdefault("magnet", []).append(titles[i].download)
             f.write(titles[i].download + "\n")
             tries += 1
     f.close()
 
     return render_template(
         "index.html",
-        len=len(magnet),
-        filename=filename,
-        size=size,
-        seeders=seeders,
-        magnet=magnet,
+        len=len(info_list["magnet"]),
+        filename=info_list["filename"],
+        size=info_list["size"],
+        seeders=info_list["seeders"],
+        magnet=info_list["magnet"],
         cover=cover,
     )
 
 
 @app.route("/torrent/<string:category>/<string:id>/", methods=["POST"])
 def mov_magnet(category, id):
-    tries = 0
     magnet_link = request.form
     for line in reversed(open("magnets.md").readlines()):
         if line.rstrip() in magnet_link["magnet"]:
             break
-        elif tries == 100:
+        else:
             return "", 404
-        tries += 1
     logged_serv.download_log(magnet_link, category)
     torrent.torrent_api(magnet_link["magnet"], category)
     return "", 204
