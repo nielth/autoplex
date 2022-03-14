a97cc2f3f1cbdfeb40027a2a8961871904868e66
nielth <thomas.nielsen98@gmail.com>
2022-03-14 04:08:59 +0100

    minor fix 2

--- app/backend/plex_check.py
+++ app/backend/plex_check.py
@@ -9,29 +9,27 @@ import os
 from dotenv import load_dotenv
 
 
-CODES_URL = 'https://plex.tv/api/v2/pins.json?strong=true'
-AUTH_URL = 'https://app.plex.tv/auth#!?{}'
-TOKEN_URL = 'https://plex.tv/api/v2/pins/{}'
+CODES_URL = "https://plex.tv/api/v2/pins.json?strong=true"
+AUTH_URL = "https://app.plex.tv/auth#!?{}"
+TOKEN_URL = "https://plex.tv/api/v2/pins/{}"
 
 PAYLOAD = {
-    'X-Plex-Product': 'Plex Auth App (Autoplex)',
-    'X-Plex-Version': '0.69.420',
-    'X-Plex-Device': 'Linux',
-    'X-Plex-Platform': 'Linux',
-    'X-Plex-Device-Name': 'Autoplex',
-    'X-Plex-Device-Vendor': '',
-    'X-Plex-Model': '',
-    'X-Plex-Client-Platform': '',
-    'X-Plex-Client-Identifier': str(uuid.uuid4())
+    "X-Plex-Product": "Plex Auth App (Autoplex)",
+    "X-Plex-Version": "0.69.420",
+    "X-Plex-Device": "Linux",
+    "X-Plex-Platform": "Linux",
+    "X-Plex-Device-Name": "Autoplex",
+    "X-Plex-Device-Vendor": "",
+    "X-Plex-Model": "",
+    "X-Plex-Client-Platform": "",
+    "X-Plex-Client-Identifier": str(uuid.uuid4()),
 }
 
 identifier = int
-parameters = {
-    'clientID': PAYLOAD['X-Plex-Client-Identifier'],
-    'code': ''
-}
+parameters = {"clientID": PAYLOAD["X-Plex-Client-Identifier"], "code": ""}
 
 s = None
+
 
 def get_plex_link(forward_url=None):
     global s
@@ -39,27 +37,29 @@ def get_plex_link(forward_url=None):
     s = requests.Session()
     resp = s.post(CODES_URL, data=PAYLOAD, headers=None)
     response = resp.json()
-    parameters['code'] = response['code']
-    identifier = response['id']
+    parameters["code"] = response["code"]
+    identifier = response["id"]
     if forward_url:
-        parameters['forwardUrl'] = forward_url
+        parameters["forwardUrl"] = forward_url
     url = AUTH_URL.format(urllib.parse.urlencode(parameters))
     return url
 
+
 def response_func():
     global TOKEN
-    token_url = TOKEN_URL.format(parameters['code'])
+    token_url = TOKEN_URL.format(parameters["code"])
     payload = dict(PAYLOAD)
-    payload['Accept'] = 'application/json'
+    payload["Accept"] = "application/json"
     resp = s.get(TOKEN_URL.format(identifier), headers=payload)
     response = resp.json()
-    TOKEN = response['authToken']
+    TOKEN = response["authToken"]
     return TOKEN
+
 
 def return_token():
     token = None
     break_loop = False
-    timeout = time.time() + 60*2
+    timeout = time.time() + 60 * 2
     while not break_loop:
         time.sleep(3)
         token = response_func()
@@ -69,7 +69,7 @@ def return_token():
     return token
 
 
-PLEX_API_BASE_URL='https://plex.tv/users/account/?X-Plex-Token='
+PLEX_API_BASE_URL = "https://plex.tv/users/account/?X-Plex-Token="
 TOKEN = None
 
 
@@ -77,22 +77,20 @@ def check_plex_user():
     global TOKEN
     xml = requests.get(PLEX_API_BASE_URL + TOKEN)
     xml_parsed = ET.fromstring(xml.content)
-    email = xml_parsed.attrib['email']
-    id = xml_parsed.attrib['id']
-    uuid = xml_parsed.attrib['uuid']
-    username = xml_parsed.attrib['username']
-    return { 'email': email, 'id': id, 'uuid': uuid, 'username': username }
+    email = xml_parsed.attrib["email"]
+    id = xml_parsed.attrib["id"]
+    uuid = xml_parsed.attrib["uuid"]
+    username = xml_parsed.attrib["username"]
+    return {"email": email, "id": id, "uuid": uuid, "username": username}
 
 
 def get_server_accounts():
     headers = {"Accept": "application/json"}
-    server_token = os.getenv('SERVER_TOKEN')
+    server_token = os.getenv("SERVER_TOKEN")
     accounts_link = f"http://10.0.0.33:32400/accounts/?X-Plex-Token={server_token}"
     resp = requests.get(accounts_link, headers=headers)
     response = resp.json()
     users = list()
-    for user in response['MediaContainer']['Account']:
-        users.append(user['name'])
+    for user in response["MediaContainer"]["Account"]:
+        users.append(user["name"])
     return users
-    
-
--- app/backend/torrent.py
+++ app/backend/torrent.py
@@ -9,10 +9,10 @@ load_dotenv()
 
 # instantiate a Client using the appropriate WebUI configuration
 qbt_client = qbittorrentapi.Client(
-    host=os.getenv('ADDRESS'),
-    port=os.getenv('PORT'),
-    username=os.getenv('QBITTORRENT_USER'),
-    password=os.getenv('QBITTORRENT_PASS'),
+    host=os.getenv("ADDRESS"),
+    port=os.getenv("PORT"),
+    username=os.getenv("QBITTORRENT_USER"),
+    password=os.getenv("QBITTORRENT_PASS"),
 )
 
 lock = threading.Lock()
@@ -21,21 +21,21 @@ lock = threading.Lock()
 def torrent_api(magnet, category):
     if category == "movie":
         qbt_client.torrents_add(
-            magnet, save_path=os.getenv('MOVIE_PATH'), is_sequential_download=True
+            magnet, save_path=os.getenv("MOVIE_PATH"), is_sequential_download=True
         )
     elif category == "series":
         qbt_client.torrents_add(
-            magnet, save_path=os.getenv('TV_PATH'), is_sequential_download=True
+            magnet, save_path=os.getenv("TV_PATH"), is_sequential_download=True
         )
 
 
 def delete_finished():
     while True:
         time.sleep(30)
-        #with lock:
-            #for torrent in qbt_client.torrents_info():
-                # check if torrent is downloading
-                #if torrent.state_enum.is_complete:
-                #    qbt_client.torrents_delete(
-                #        delete_files=False, torrent_hashes=torrent.hash
-                #    )
+        # with lock:
+        # for torrent in qbt_client.torrents_info():
+        # check if torrent is downloading
+        # if torrent.state_enum.is_complete:
+        #    qbt_client.torrents_delete(
+        #        delete_files=False, torrent_hashes=torrent.hash
+        #    )
