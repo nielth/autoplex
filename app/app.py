e36af510ba1675e6f84955e860452d37829d980b
nielth <thomas.nielsen98@gmail.com>
2022-03-14 04:08:59 +0100

    minor fix 2

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from flask_login import (
     LoginManager,
     login_required,
     current_user,
-    logout_user
+    logout_user,
 )
 
 import _thread
@@ -23,19 +23,19 @@ import backend.plex_check as plex_check
 load_dotenv()
 
 app = Flask(__name__)
-app.config['ENV'] = 'development'
+app.config["ENV"] = "development"
 # python -c 'import secrets; print(secrets.token_hex())'
-app.config['SECRET_KEY'] = os.getenv('SECRET_FLASK_KEY')
-app.config['SESSION_COOKIE_SECURE'] = False
-app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config["SECRET_KEY"] = os.getenv("SECRET_FLASK_KEY")
+app.config["SESSION_COOKIE_SECURE"] = False
+app.config["SESSION_COOKIE_HTTPONLY"] = True
 
 client = rarbgapi.RarbgAPI()
 ia = IMDb()
 series = "tv series"
 movie = "movie"
 
-ADDRESS = os.getenv('ADDRESS') 
-PORT = os.getenv('APP_PORT')
+ADDRESS = os.getenv("ADDRESS")
+PORT = os.getenv("APP_PORT")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -47,8 +47,8 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.url_map.strict_slashes = False
 
 db = SQLAlchemy(app)
@@ -60,19 +60,21 @@ class User(UserMixin, db.Model):
     uuid = db.Column(db.String(100), nullable=False, unique=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
 
+
 db.create_all()
 
 FORWARD_LINK = None
 PAYLOAD = {
-    'X-Plex-Product': 'Test Product',
-    'X-Plex-Version': '0.0.1',
-    'X-Plex-Device': 'Test Device',
-    'X-Plex-Platform': 'Test Platform',
-    'X-Plex-Device-Name': 'Test Device Name',
-    'X-Plex-Device-Vendor': 'Test Vendor',
-    'X-Plex-Model': 'Test Model',
-    'X-Plex-Client-Platform': 'Test Client Platform'
+    "X-Plex-Product": "Test Product",
+    "X-Plex-Version": "0.0.1",
+    "X-Plex-Device": "Test Device",
+    "X-Plex-Platform": "Test Platform",
+    "X-Plex-Device-Name": "Test Device Name",
+    "X-Plex-Device-Vendor": "Test Vendor",
+    "X-Plex-Model": "Test Model",
+    "X-Plex-Client-Platform": "Test Client Platform",
 }
+
 
 @app.route("/", methods=["GET"])
 @login_required
@@ -83,46 +85,49 @@ def index():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
 @app.errorhandler(401)
 def page_not_found(e):
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
-@app.route("/login", methods=['GET'])
+@app.route("/login", methods=["GET"])
 def login():
-    FORWARD_LINK = plex_check.get_plex_link(forward_url=f"http://{ADDRESS}:{PORT}/login/callback")
+    FORWARD_LINK = plex_check.get_plex_link(
+        forward_url=f"http://{ADDRESS}:{PORT}/login/callback"
+    )
     return f'<a class="btn btn-success" href="{FORWARD_LINK}" target="_blank">Continue with Plex</a>'
 
 
-@app.route("/login/callback", methods=['GET'])
+@app.route("/login/callback", methods=["GET"])
 def callback():
     token = plex_check.return_token()
     plex_user_info = plex_check.check_plex_user()
     plex_server_users = plex_check.get_server_accounts()
-    username = plex_user_info['username'] in plex_server_users
-    email = plex_user_info['email'] in plex_server_users
+    username = plex_user_info["username"] in plex_server_users
+    email = plex_user_info["email"] in plex_server_users
     if not any((username, email)):
-        return 'User is not linked to Plex Server', 404
+        return "User is not linked to Plex Server", 404
 
-    user = User.query.filter_by(email=plex_user_info['email']).first()
+    user = User.query.filter_by(email=plex_user_info["email"]).first()
     if user:
         login_user(user)
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
         new_user = User(
-            email = plex_user_info['email'],
-            id = plex_user_info['id'],
-            uuid = plex_user_info['uuid'],
-            username = plex_user_info['username']
+            email=plex_user_info["email"],
+            id=plex_user_info["id"],
+            uuid=plex_user_info["uuid"],
+            username=plex_user_info["username"],
         )
         db.session.add(new_user)
         db.session.commit()
-        user = User.query.filter_by(email=plex_user_info['email']).first()
+        user = User.query.filter_by(email=plex_user_info["email"]).first()
         login_user(user)
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
+
 
 @app.route("/search/<string:category>/<string:title_search>", methods=["GET"])
 @login_required
@@ -143,9 +148,11 @@ def search(category, title_search):
         ):
             result = movie[i].data["cover url"].find("._V1_")
             info_list.setdefault("title", []).append(movie[i].data["title"])
-            info_list.setdefault("cover", []).append(movie[i]
-                                                     .data["cover url"]
-                                                     .replace(movie[i].data["cover url"][result + 3: result + 23], ""))
+            info_list.setdefault("cover", []).append(
+                movie[i]
+                .data["cover url"]
+                .replace(movie[i].data["cover url"][result + 3 : result + 23], "")
+            )
             info_list.setdefault("id", []).append(movie[i].movieID)
             titles += 1
     return render_template(
@@ -190,7 +197,7 @@ def get_torrents(name, category):
     title = ia.get_movie(name)
     result = title.data["cover url"].find("._V1_")
     cover = title.data["cover url"].replace(
-        title.data["cover url"][result + 3: result + 23], ""
+        title.data["cover url"][result + 3 : result + 23], ""
     )
     torrent_search = "tt" + name
     titles = get_magnet(torrent_search, category)
@@ -207,17 +214,17 @@ def get_torrents(name, category):
             tries = 0
             break
 
-    f = open("../log/magnets.md", "a")
-    for i in range(len(titles)):
-        if titles[i].seeders != 0:
-            info_list.setdefault("filename", []).append(titles[i].filename)
-            info_list.setdefault("size", []).append(
-                round((titles[i].size / 1073741824), 2))
-            info_list.setdefault("seeders", []).append(titles[i].seeders)
-            info_list.setdefault("magnet", []).append(titles[i].download)
-            f.write(titles[i].download + "\n")
-            tries += 1
-    f.close()
+    with open("backend/log/magnets.md", "a") as f:
+        for i in range(len(titles)):
+            if titles[i].seeders != 0:
+                info_list.setdefault("filename", []).append(titles[i].filename)
+                info_list.setdefault("size", []).append(
+                    round((titles[i].size / 1073741824), 2)
+                )
+                info_list.setdefault("seeders", []).append(titles[i].seeders)
+                info_list.setdefault("magnet", []).append(titles[i].download)
+                f.write(titles[i].download + "\n")
+                tries += 1
 
     return render_template(
         "index.html",
@@ -234,7 +241,7 @@ def get_torrents(name, category):
 @login_required
 def mov_magnet(category, id):
     magnet_link = request.form
-    for line in reversed(open("./log/magnets.md").readlines()):
+    for line in reversed(open("backend/log/magnets.md").readlines()):
         if line.rstrip() in magnet_link["magnet"]:
             break
         else:
@@ -245,5 +252,5 @@ def mov_magnet(category, id):
 
 
 if __name__ == "__main__":
-    #_thread.start_new_thread(torrent.delete_finished, ())
+    # _thread.start_new_thread(torrent.delete_finished, ())
     app.run(host="0.0.0.0", debug=True, threaded=True)
