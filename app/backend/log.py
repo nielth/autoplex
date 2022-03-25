37ba862a8dfaf6a53cfccd50e4e4550dc0dd8f35
Thomas Nielsen <thomas.nielsen98@gmail.com>
2022-03-25 01:47:55 +0000

    Server fixes

--- app/backend/log.py
+++ app/backend/log.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 import json
+import os
 
 
 def download_log(magnet_link, category):
-    with open("backend/log/downloaded.json", "r+") as file:
+    script_dir = os.path.dirname(__file__)
+    abs_path = os.path.join(script_dir, "log")
+    if not os.path.exists(abs_path):
+        os.makedirs(abs_path)
+    with open(abs_path + "/downloaded.json", "a") as file:
         logged_download = {
             "time": datetime.today().strftime("%H:%M:%S"),
             "category": category,
@@ -15,7 +20,7 @@ def download_log(magnet_link, category):
             file_data[datetime.today().strftime("%Y-%m-%d")].append(logged_download)
             file.seek(0)
             json.dump(file_data, file, indent=4)
-        except json.decoder.JSONDecodeError:
+        except:
             logged_download_new = {
                 datetime.today().strftime("%Y-%m-%d"): [logged_download],
             }
