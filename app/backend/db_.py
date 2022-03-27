42c464d8729e629d1381cc4bad65546527b63c09
nielth <thomas.nielsen98@gmail.com>
2022-03-27 03:59:03 +0200

    added registration

--- app/backend/db_.py
+++ app/backend/db_.py
@@ -4,7 +4,8 @@ from app import db, UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    uuid = db.Column(db.String(100), nullable=False, unique=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
+    uuid = db.Column(db.String(100), nullable=True, unique=True)
+    username = db.Column(db.String(100), nullable=True, unique=True)
+    password = db.Column(db.String(100), nullable=True, unique=True)
 
 db.create_all()
