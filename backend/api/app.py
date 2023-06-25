516095130d2bafbae9948e48c084408bb6b4bf55
nielth <thomas.nielsen98@gmail.com>
2023-06-26 01:38:47 +0200

    login workin

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,18 +1,43 @@
+import requests
+import xmltodict
+
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import abort
+
 from flask_cors import CORS
 
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import set_access_cookies
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
+plex_auth = "https://plex.tv/users/account/?X-Plex-Token="
+
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, supports_credentials=True)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
+
 
 # Setup the Flask-JWT-Extended extension
+app.config["JWT_TOKEN_LOCATION"] = [
+    "headers", "cookies", "json", "query_string"]
+# If true this will only allow the cookies that contain your JWTs to be sent
+# over https. In production, this should always be set to True
+app.config["JWT_COOKIE_SECURE"] = False
+
 app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+
 jwt = JWTManager(app)
 
 
@@ -38,11 +63,60 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
+
+def req(url):
+    try:
+        r = requests.get(
+            url, timeout=1, verify=True)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error")
+        print(errh.args[0])
+    except requests.exceptions.ReadTimeout as errrt:
+        print("Time out")
+    except requests.exceptions.ConnectionError as conerr:
+        print("Connection error")
+    except requests.exceptions.RequestException as errex:
+        print("Exception request")
+    return r
+
+
 @app.route("/authToken", methods=["POST"])
 def temp():
     data = request.json
-    print(data)
-    return data
+    if not "authToken" in data:
+        return ""
+
+    user_req = req(plex_auth + data['authToken'])
+
+    if user_req.status_code == 200:
+        user_dump = xmltodict.parse(user_req.content)
+    else:
+        return ""
+    if user_dump['user']['@id'] and user_dump['user']['@username']:
+        user = user_dump['user']['@username']
+        uid = user_dump['user']['@id']
+    elif user_dump['user']['@id']:
+        uid = user_dump['user']['@id']
+    else:
+        return ""
+
+    server_req = req(
+        "http://193.29.107.103:38983/accounts/?X-Plex-Token=nWWbRwG4matE1jXjBimL")
+    if server_req.status_code == 200:
+        server_dump = xmltodict.parse(server_req.content)
+    else:
+        return ""
+
+    for server_user in server_dump['MediaContainer']['Account']:
+        if uid == server_user['@id'] or user == server_user['@name']:
+            print("Success!")
+            response = jsonify({"msg": "login successful"})
+            access_token = create_access_token(identity=user)
+            set_access_cookies(response, access_token)
+            return response
+
+    return jsonify({"msg": "User not connected to Plex server"}), 401
 
 
 if __name__ == "__main__":
--- frontend/src/pages/callback.tsx
+++ frontend/src/pages/callback.tsx
@@ -2,6 +2,7 @@ import { Box, Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { PAYLOAD } from "../components/payload";
+import { getCookie } from "../components/getCookie";
 
 export function Callback() {
   const [items, setItems] = useState([]);
@@ -23,15 +24,22 @@ export function Callback() {
     axios
       .get(url_auth_code, { headers: PAYLOAD })
       .then((res) => {
-        localStorage.setItem("authToken", res.data.authToken);
         authToken = res.data.authToken;
-        axios.post(
-          "http://192.168.0.165:5000/authToken",
-          { authToken: authToken },
-          {
-            headers: {},
-          }
-        );
+        axios
+          .post(
+            "http://192.168.0.165:5000/authToken",
+            { authToken: authToken },
+            {
+              withCredentials: true,
+              headers: { "Access-Control-Allow-Origin": "*" },
+            }
+          )
+          .then(() => {
+            axios.get("http://192.168.0.165:5000/protected", {
+              withCredentials: true,
+              headers: { "X-CSRF-TOKEN": getCookie("csrf_access_token") },
+            });
+          });
       })
       .catch((err) => {
         console.log(err);
