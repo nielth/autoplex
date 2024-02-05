3240b6cc278d63149d5357643d062200303a80d2
nielth <thomas.nielsen98@gmail.com>
2024-02-05 22:04:46 +0100

    initial

--- frontend/src/pages/Callback.tsx
+++ frontend/src/pages/Callback.tsx
@@ -1,25 +1,8 @@
-import { useEffect } from "react";
-import { login } from "../lib/api";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export function Callback() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    (async () => {
-      let url_auth = "https://plex.tv/api/v2/pins/";
-      let url_auth_code = "";
-      const lstore = JSON.parse(localStorage.getItem("response") || "{}");
-      const payload = JSON.parse(localStorage.getItem("payload") || "{}");
-      payload["X-Plex-Client-Identifier"] = lstore.clientIdentifier;
-      url_auth_code = url_auth + lstore.id;
-
-      await login(url_auth_code, payload).then((answer) => {
-        console.log(answer);
-        navigate("/", { replace: true });
-      });
-    })();
-  }, []);
-
-  return <div>Redirecting...</div>;
+export default function Callback() {
+  axios.get("/api/callback").then((resp) => {
+    console.log(resp.data);
+  });
+  return <>Callback</>;
 }
