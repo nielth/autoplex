6d8f97cdc92533a4556c3e4bf850d0745b16f3b4
nielth <thomas.nielsen98@gmail.com>
2024-02-05 22:04:46 +0100

    initial

--- frontend/prod.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:18-alpine AS builder
-WORKDIR /app
-COPY . .
-RUN yarn  
-RUN yarn build
-
-FROM nginx:alpine
-COPY --from=builder /app/build /usr/share/nginx/html
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,76 +1,18 @@
-import { Box, Button, CardMedia, Container, Typography } from "@mui/material";
-import { oauthPlexLink } from "../lib/api";
-import { getWithExpiry } from "../utils/localStorExpire";
 import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../App";
+import axios from "axios";
 
-async function setUrlFunc() {
-  const url = await getWithExpiry("url");
-  if (!url) {
-    await oauthPlexLink();
-  }
-  return url;
-}
+export default function LoginPage() {
+  const [error, setError] = useState<string>();
 
-// backgroundImage: 'url(https://media.giphy.com/media/3o7aDdlFRJU8D4F7QQ/giphy.gif);'
-export function Login(context: any) {
-  const [url, setUrl] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
 
-  setUrlFunc().then((resp) => {
-    setUrl(resp);
-  });
+  let from = location.state?.from?.pathname || "/";
 
-  return (
-    <>
-      <Container
-        maxWidth="lg"
-        sx={{
-          pt: { xs: "50px", lg: "100px" },
-        }}
-      >
-        <Typography
-          sx={{
-            backgroundImage: "url('text-animation.gif')",
-            textAlign: "center",
-            backgroundSize: "cover",
-            color: "transparent",
-            backgroundClip: "text",
-            fontWeight: "900",
-            fontSize: { xs: "45px", lg: "100px" },
-          }}
-        >
-          AUTOPLEX
-        </Typography>
+  axios.get("/api/authToken")
 
-        <Box
-          height={1}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: {xs: '270px', lg: '320px'},
-            backgroundColor: {xs: 'inherit', lg:"secondary.main"},
-            border: "7px solid",
-            borderWidth: "1px",
-            borderColor: "border.main",
-            minHeight: "300px",
-            margin: "auto",
-            borderRadius: "10px",
-            mt: '70px'
-          }}
-        >
-          <Button
-            variant="outlined"
-            onClick={function () {
-              localStorage.removeItem("url");
-            }}
-            href={url}
-            size="large"
-            sx={{ color: "plex.main", borderColor: "border.main" }}
-          >
-            Continue with Plex
-          </Button>
-        </Box>
-      </Container>
-    </>
-  );
+  return <>Not logged in</>;
 }
