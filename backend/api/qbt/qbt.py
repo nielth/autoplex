f6bdf7e86bc6810518b8d02a9d5ebd64513aee38
nielth <thomas.nielsen98@gmail.com>
2024-02-09 16:48:08 +0100

    working

--- backend/api/qbt/qbt.py
+++ backend/api/qbt/qbt.py
@@ -2,17 +2,17 @@ import os
 
 from qbittorrent import Client
 
-USER = os.getenv('QBT_USER')
-PASSWORD = os.getenv('QBT_PASS')
+USER = os.getenv("QBT_USER")
+PASSWORD = os.getenv("QBT_PASS")
 
 
 def authenticate():
-    qb = Client('http://192.168.0.165:8080/')
+    qb = Client("https://qbt.internal.nielth.com/", verify=False)
     qb.login(USER, PASSWORD)
     return qb
 
 
 def qbtTorrentDownload(torrent_file):
     qb = authenticate()
-    dl_path = '/downloads'
+    dl_path = "/downloads/sdb/tvseries"
     qb.download_from_file(torrent_file, savepath=dl_path)
