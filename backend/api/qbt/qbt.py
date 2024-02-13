2be711500035749fdb8123635e3b08c4d4991694
Junta <thomas.nielsen98@gmail.com>
2024-02-13 08:33:33 +0000

    readme, fixed env shit

--- backend/api/qbt/qbt.py
+++ backend/api/qbt/qbt.py
@@ -10,7 +10,7 @@ PASSWORD = os.getenv("QBT_PASS")
 
 def get_info_hash_v1(torrent_file):
     torrent_data = bencodepy.decode(torrent_file)
-    info = torrent_data[b'info']
+    info = torrent_data[b"info"]
     info_hash = hashlib.sha1(bencodepy.encode(info)).hexdigest()
     return info_hash
 
@@ -27,4 +27,3 @@ def qbtTorrentDownload(torrent_file):
     qb.download_from_file(torrent_file, savepath=dl_path)
     torrent_hash = get_info_hash_v1(torrent_file)
     qb.toggle_sequential_download(torrent_hash)
-
