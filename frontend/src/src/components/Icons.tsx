c14e24d6432f24619e41a8ebed260560812da16c
Thomas Nielsen <thomas.nielsen98@gmail.com>
2024-10-06 12:01:05 +0200

    pagination working, search feedback working

---
+++ frontend/src/src/components/Icons.tsx
@@ -0,0 +1,92 @@
+export function TimestampIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-clock"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+    </svg>
+  );
+}
+
+export function SeedersIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-caret-up"
+      viewBox="0 0 16 16"
+    >
+      <path d="M3.204 11h9.592L8 5.519zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659" />
+    </svg>
+  );
+}
+
+export function LeechersIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-caret-down"
+      viewBox="0 0 16 16"
+    >
+      <path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659" />
+    </svg>
+  );
+}
+
+export function SizeIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-files"
+      viewBox="0 0 16 16"
+    >
+      <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1M3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1z" />
+    </svg>
+  );
+}
+
+export function CompletedIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-download"
+      viewBox="0 0 16 16"
+    >
+      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+    </svg>
+  );
+}
+
+export function ButtonDownloadIcon(props: { width: string; height: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.width}
+      height={props.height}
+      fill="currentColor"
+      className="bi bi-download"
+      viewBox="0 0 16 16"
+    >
+      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+    </svg>
+  );
+}
--- frontend/src/src/routes/Home.tsx
+++ frontend/src/src/routes/Home.tsx
@@ -1,17 +1,50 @@
 import { useEffect, useRef, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { temp } from "./temp";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { TorrentList } from "../components/TorrentList";
+import axios from "axios";
+import { authProvider } from "../auth";
+
+async function search_torrent(
+  search: string,
+  page: number,
+  navigate: Function,
+  setData: Function,
+  setDataLoading: Function
+) {
+  await setDataLoading(true);
+  const answer = await axios
+    .get(`http://localhost:5050/api/search/${search}/${page}`, {
+      withCredentials: true,
+    })
+    .then((resp) => {
+      if (resp.status && resp.status === 200) {
+        setData(resp.data);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.status === 401) {
+        authProvider.signout();
+        navigate("/login");
+      } else {
+        navigate("/error");
+      }
+    });
+  await setDataLoading(false);
+  return answer;
+}
 
 export function Home() {
   const [search, setSearch] = useState<string>("");
   const [data, setData] = useState<TorrentData>(Object);
   const [page, setPage] = useState<number>(1);
+  const [dataLoading, setDataLoading] = useState<Boolean>(false);
   const inputRef = useRef<HTMLInputElement>(null);
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   async function pagination(next_or_prev: boolean, old_page: number) {
-    console.log(old_page);
+    setData(Object());
     if (
       next_or_prev == true &&
       old_page < Math.ceil(data.numFound / data.perPage)
@@ -28,21 +61,28 @@ export function Home() {
     if (event.key === "Enter" || event.type === "click") {
       setSearchParams(`search=${search}&p=1`);
       setPage(1);
+      search_torrent(search, 1, navigate, setData, setDataLoading);
     }
   };
 
   useEffect(() => {
     const sparmSearch = searchParams.get("search");
     const sparmPage = searchParams.get("p");
-    setData(temp);
-    console.log(temp);
 
     if (sparmSearch !== null && sparmPage !== null) {
       setSearch(sparmSearch);
       setPage(Number(sparmPage));
+      search_torrent(
+        sparmSearch,
+        Number(sparmPage),
+        navigate,
+        setData,
+        setDataLoading
+      );
     } else {
       setSearch("");
       setPage(1);
+      setData(Object());
     }
   }, [searchParams]);
 
@@ -51,6 +91,10 @@ export function Home() {
       inputRef.current.focus();
     }
   });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <>
@@ -66,51 +110,58 @@ export function Home() {
           value={search}
           className="input input-bordered w-full max-w-md"
         />
-        <button onClick={handleSearch} className="btn btn-outline">
+        <button
+          onClick={handleSearch}
+          className="hidden md:block btn btn-outline"
+        >
           Search
         </button>
       </div>
       <div>
-        {data && data.torrentList ? (
-          <div className="mx-auto text-center">
-            <TorrentList data={data} />
-            <div className="join">
-              {page <= 1 ? (
-                <button
-                  className="join-item btn btn-disabled"
-                  onClick={() => pagination(false, page)}
-                >
-                  «
-                </button>
-              ) : (
-                <button
-                  className="join-item btn"
-                  onClick={() => pagination(false, page)}
-                >
-                  «
-                </button>
-              )}
-              <button className="join-item btn">
-                Page {page}/{Math.ceil(data.numFound / data.perPage)}
-              </button>
-              {page >= Math.ceil(data.numFound / data.perPage) ? (
-                <button
-                  className="join-item btn btn-disabled"
-                  onClick={() => pagination(true, page)}
-                >
-                  »
-                </button>
-              ) : (
-                <button
-                  className="join-item btn"
-                  onClick={() => pagination(true, page)}
-                >
-                  »
-                </button>
-              )}
+        <div className="pt-8">
+          {dataLoading ? (
+            <div>
+              {[...Array(35).keys()].map((key) => (
+                <div>
+                  <div className="divider my-2" />
+                  <div className="skeleton h-12 w-full"></div>
+                </div>
+              ))}
             </div>
-          </div>
-        ) : null}
+          ) : null}
+          {data.torrentList ? (
+            <div className="mx-auto">
+              <TorrentList data={data} />
+              <div className="text-center">
+                <div className="join">
+                  {page <= 1 ? (
+                    <button className="join-item btn btn-disabled">«</button>
+                  ) : (
+                    <button
+                      className="join-item btn"
+                      onClick={() => pagination(false, page)}
+                    >
+                      «
+                    </button>
+                  )}
+                  <button className="join-item btn">
+                    Page {page}/{Math.ceil(data.numFound / data.perPage)}
+                  </button>
+                  {page >= Math.ceil(data.numFound / data.perPage) ? (
+                    <button className="join-item btn btn-disabled">»</button>
+                  ) : (
+                    <button
+                      className="join-item btn"
+                      onClick={() => pagination(true, page)}
+                    >
+                      »
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
     </>
   );
