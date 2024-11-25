bb392c421090a965598b70e6d4c61cf74dfa2279
Thomas Nielsen <thomas.nielsen98@gmail.com>
2024-11-25 14:42:57 +0100

    Comments, code cleanup, EdDSA/ed25519 algoritm implemented

--- backend/api/services/jwt.go
+++ backend/api/services/jwt.go
@@ -1,31 +1,51 @@
 package services
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secretKey = []byte("CHANGE_PROD")
+var (
+	privateKey ed25519.PrivateKey
+	publicKey  ed25519.PublicKey
+)
+
+func init() {
+	// Generate Ed25519 key pair dynamically
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		panic("Error generating Ed25519 key pair: " + err.Error())
+	}
+	privateKey = priv
+	publicKey = pub
+}
 
 func CreateToken(username string) (string, error) {
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	claims := jwt.NewWithClaims(jwt.SigningMethodEdDSA, jwt.MapClaims{
 		"sub": username,
 		"exp": time.Now().Add(time.Hour * 24 * 7).Unix(),
 		"iat": time.Now().Unix(),
 	})
 
-	return claims.SignedString(secretKey)
+	return claims.SignedString(privateKey)
 }
 
 func VerifyToken(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return secretKey, nil
+		// Ensure the signing method is EdDSA
+		if _, ok := token.Method.(*jwt.SigningMethodEd25519); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return publicKey, nil
 	})
 
 	if err != nil || !token.Valid {
-		return nil, fmt.Errorf("invalid token")
+		return nil, errors.New("invalid token")
 	}
 
 	return token, nil
--- backend/api/services/plex.go
+++ backend/api/services/plex.go
@@ -26,11 +26,32 @@ func GetPlexToken() string {
 	return os.Getenv("PLEX_TOKEN")
 }
 
+func ReturnPlexAuthPayload(client_identifer string) map[string]string {
+	header := map[string]string{
+		"x-plex-product":           "plex auth app (autoplex)",
+		"x-plex-version":           "0.69.420",
+		"x-plex-device":            "linux",
+		"x-plex-platform":          "linux",
+		"x-plex-device-name":       "autoplex",
+		"x-plex-device-vendor":     "test",
+		"x-plex-model":             "",
+		"x-plex-client-platform":   "",
+		"x-plex-client-identifier": client_identifer,
+		"content-type":             "application/json",
+		"Accept":                   "application/json",
+	}
+
+	return header
+
+}
+
+// Function to request a generated, temporary Plex oauth link with a unique locally generated UUID4
 func InitAuth() (string, int, string) {
-	id := uuid.New().String()
-	header := ReturnPlexAuthPayload(id)
+	localUUID := uuid.New().String()
+	header := ReturnPlexAuthPayload(localUUID)
 	client := http.Client{}
 
+	// Request a temporary response ID from plex for oauth authentication where a uuid4 is used to to verify this transaction
 	req, _ := http.NewRequest("POST", "https://plex.tv/api/v2/pins.json?strong=true", nil)
 	for k, v := range header {
 		req.Header.Set(k, v)
@@ -46,14 +67,17 @@ func InitAuth() (string, int, string) {
 	var result map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&result)
 
-	code := result["code"].(string)
+	// We us our locally generated uuid4 as well as the returned plex code to create the url to authenticate the user
+	returnedPlexCode := result["code"].(string)
 	respID := int(result["id"].(float64))
-	authURL := fmt.Sprintf("https://app.plex.tv/auth#!?clientID=%s&code=%s", id, code)
+	authURL := fmt.Sprintf("https://app.plex.tv/auth#!?clientID=%s&code=%s", localUUID, returnedPlexCode)
 
-	return authURL, respID, id
+	return authURL, respID, localUUID
 }
 
+// Function to check the oauth transaction, if true, check if user is linked to local Plex server
 func RequestAuthToken(respID string, clientID string) (string, bool) {
+	// Link to get authToken from user oauth transaction
 	header := ReturnPlexAuthPayload(clientID)
 	tokenUrl := fmt.Sprintf("https://plex.tv/api/v2/pins/%s", respID)
 
@@ -83,7 +107,9 @@ func RequestAuthToken(respID string, clientID string) (string, bool) {
 		fmt.Println("Could not receive 'authToken'")
 	}
 
+	// Link to get basic information about user oauth session to check if user is linked to plex
 	url1 := fmt.Sprintf("https://plex.tv/users/account/?X-Plex-Token=%s", token)
+	// Link to local Plex server to get all users linked in order to validate user login request (might take a while for user to appear)
 	url2 := fmt.Sprintf("http://nielth.com:32444/accounts/?X-Plex-Token=%s", plexToken)
 
 	errCh := make(chan error, 2)
@@ -116,23 +142,4 @@ func RequestAuthToken(respID string, clientID string) (string, bool) {
 		return user.Username, true
 	}
 	return "", false
-}
-
-func ReturnPlexAuthPayload(client_identifer string) map[string]string {
-	header := map[string]string{
-		"x-plex-product":           "plex auth app (autoplex)",
-		"x-plex-version":           "0.69.420",
-		"x-plex-device":            "linux",
-		"x-plex-platform":          "linux",
-		"x-plex-device-name":       "autoplex",
-		"x-plex-device-vendor":     "test",
-		"x-plex-model":             "",
-		"x-plex-client-platform":   "",
-		"x-plex-client-identifier": client_identifer,
-		"content-type":             "application/json",
-		"Accept":                   "application/json",
-	}
-
-	return header
-
 }
